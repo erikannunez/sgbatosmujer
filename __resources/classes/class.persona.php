73352422ad983e9cdb6b56908089ca4e24b91a6b
erikannunez <erica_n_nunez@aysa.com.ar>
2016-10-17 15:50:44 -0300

    codeigniter

---
+++ __resources/classes/class.persona.php
@@ -0,0 +1,17 @@
+<?php
+class Persona{
+	
+	private $id;
+	private $legajo;
+	private $apellido;
+	private $nombre;
+	private $fnac;
+	private $emails;
+	
+	function __construct(){
+		
+	}
+		
+		
+}
+?>
